97097329d99f4bae69ec984cf1426854f006072f
LAripping <leo.tsaou@gmail.com>
2022-08-24 21:47:54 +0300

    Made JS code better

--- menu.js
+++ menu.js
@@ -18,28 +18,12 @@ width > 768:    Expand normally
 if (window_width >= 768) {
     ssb_panel.onmouseenter = function () {
         if (ssb_panel.classList.contains("ssb-btns-right") && ssb_panel.classList.contains("ssb-anim-slide")) {
-            var animation = ssb_panel.animate(
-                [{right: 0}], 
-                {duration: 300,easing: 'ease-in-out',iterations: 1}
-            )
-            animation.onfinish = function () {
-                animation.cancel();
-                ssb_panel.style.right = 0;
-           }
+            slide('out');
         }
     };
     ssb_panel.onmouseleave =  function () {
         if (ssb_panel.classList.contains("ssb-btns-right") && ssb_panel.classList.contains("ssb-anim-slide")) {
-            // ssb_panel.animate({'right': '-' + (ssb_panel_w - sbb_display_margin) + 'px'}, 300);
-            var offset = '-'+(ssb_panel_w - sbb_display_margin) + 'px';
-            var animation = ssb_panel.animate(
-                [{right: offset}], 
-                {duration: 300,easing: 'ease-in-out',iterations: 1}
-            )
-            animation.onfinish = function () {
-                animation.cancel();
-                ssb_panel.style.right = offset;
-           }
+            slide('in');
         }
 
     };
@@ -49,32 +33,31 @@ if (window_width >= 768) {
         if (this.classList.contains("ssb-open")) {
             this.classList.remove("ssb-open");
             if (ssb_panel.classList.contains("ssb-btns-right") && ssb_panel.classList.contains("ssb-anim-slide")) {
-                var offset = '-' + (ssb_panel_w - sbb_display_margin) + 'px'
-                var animation = ssb_panel.animate(
-                    [{right: offset}],
-                    {duration: 300, easing: 'ease-in-out',iterations:1}
-                )
-                animation.onfinish = function () {
-                    animation.cancel();
-                    ssb_panel.style.right = offset;
-               }
+                slide('in');
             }
         } else {
             e.preventDefault();
             this.classList.add("ssb-open");
             if (ssb_panel.classList.contains("ssb-btns-right") && ssb_panel.classList.contains("ssb-anim-slide")) {
-                var animation = ssb_panel.animate(
-                    [{right: 0}], 
-                    {duration: 300,easing: 'ease-in-out',iterations: 1}
-                )
-                animation.onfinish = function () {
-                    animation.cancel();
-                    ssb_panel.style.right = 0;
-               }
+                slide('out');
             }
         }
     }, false);
 }
 
-
-
+function slide(dir){
+    var offset;
+    if(dir=='out'){
+        offset = 0;
+    } else if(dir=='in'){
+        offset = '-' + (ssb_panel_w - sbb_display_margin) + 'px'
+    }
+    var animation = ssb_panel.animate(
+        [{right: offset}], 
+        {duration: 300,easing: 'ease-in-out',iterations: 1}
+    )
+    animation.onfinish = function () {
+        animation.cancel();
+        ssb_panel.style.right = offset;
+   }
+}
